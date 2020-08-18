2e140d1fbf6a1997482f2b3e38049eb57c8ce3bc
julianramirezch <julianramirezch1@gmail.com>
2020-08-18 12:41:42 -0500

    [fix]: Installation Order

--- configuration.sh
+++ configuration.sh
@@ -4,11 +4,11 @@ EMAIL="$2"
 URL="$3"
 sudo apt-get -y upgrade
 sudo apt-get -y update
-sudo apt-get -y install zsh
 sudo apt-get -y install git-core
-sudo apt-get -y install shellcheck
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-zsh
 git clone $URL
 git config --global user.name "$USER"
 git config --global user.email "$EMAIL"
+sudo apt-get -y install zsh
+sudo apt-get -y install shellcheck
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+zsh
